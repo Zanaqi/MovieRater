4271a71bfc3b518d0819874578a4f4f639a7ac26
Mirza <172682Q@mymail.nyp.edu.sg>
2018-11-16 22:17:13 +0800

    rename add movie file

--- app/src/main/java/com/example/mirza/movieraterbasic/AddMovieActivity.kt
+++ app/src/main/java/com/example/mirza/movieraterbasic/AddMovieActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.RadioButton
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.add_movie.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.add_movie)
 
         suitChk1.setOnClickListener(View.OnClickListener{
 
